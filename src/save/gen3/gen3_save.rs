b27c09e0dbffc7edc7e4eebd58c3f76803605b26
Helton Reis <hrkings@rhaizzing.com>
2022-06-28 15:30:48 -0300

    [feature] Add some data structures for Gen 3 save

---
+++ src/save/gen3/gen3_save.rs
@@ -0,0 +1,63 @@
+use std::fmt::{Display, Formatter};
+
+pub enum Gen3Game {
+    RubySapphire,
+    FireRedLeafGreen,
+    Emerald,
+}
+
+impl Display for Gen3Game {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Gen3Game::RubySapphire => write!(f, "Ruby/Sapphire"),
+            Gen3Game::FireRedLeafGreen => write!(f, "FireRed/LeafGreen"),
+            Gen3Game::Emerald => write!(f, "Emerald"),
+        }
+    }
+}
+
+pub struct KeyCode {
+    pub game_code: Option<u32>,
+    pub security_key: Option<u32>,
+}
+
+pub struct PlayedTime {
+    pub time: [u8; 5],
+    pub hours: u16,
+    pub minutes: u8,
+    pub seconds: u8,
+    pub frames: u8,
+}
+
+pub struct TrainerId {
+    pub trainer_id: u32,
+    pub secret_id: u16,
+    pub public_id: u16
+}
+
+pub struct TrainerData {
+    pub section_info: Option<SectionData>,
+    pub name: [u8; 7],
+    pub gender: u8,
+    pub id: TrainerId,
+    pub time: PlayedTime,
+    pub security: KeyCode,
+}
+
+pub struct SectionData {
+    pub section_id: u16,
+    pub checksum: u16,
+    pub signature: u32,
+    pub save_index: u32,
+}
+
+impl Display for SectionData {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "ID: {}, Checksum: {}, Signature: {}, Save Index: {}", self.section_id, self.checksum, self.signature, self.save_index)
+    }
+}
+
+pub struct SaveGen3 {
+    pub game_ver: Gen3Game,
+    pub trainer_section: Option<TrainerData>,
+}
